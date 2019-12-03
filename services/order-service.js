88c18cfc171126f347d0c8c71907a55f48b2cee5
Robert <robert.malina987@gmail.com>
2019-12-03 21:49:26 +0100

    auth-related dependencies addition: bcrypt, passport & JEST (testing)

--- services/order-service.js
+++ services/order-service.js
@@ -5,11 +5,17 @@ const OrderService = function() {
   const db = new DbAccess();
 
   this.fetchOrders = function() {
-    return db.run('select * from [Order]');
+    return db.run('select * from [Order]')
+      .catch((error)=> {
+        console.error(error);
+      });
   };
 
   this.fetchOrder = function(id) {
-    return db.run(`select * from [Order] where [Id] = ${id}`);
+    return db.run(`select * from [Order] where [Id] = ${id}`)
+      .catch((error)=> {
+        console.error(error);
+      });
   };
 };
 
