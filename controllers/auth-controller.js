0571d9b7c23df22434a086ab7c4e557b483c58e3
Robert <robert.malina987@gmail.com>
2019-12-04 19:20:52 +0100

    complete,serverside, async user registration

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,12 +1,26 @@
 const Controller = require('./base/controller');
 const Action = require('./base/action');
 
-const AuthController = function(/*OrderService class*/ userService) {
+const AuthController = function(/*AuthService class*/ authService) {
   
-  Controller.call(this);
+  Controller.call(authService);
 
   const service = userService;
 
-  this.registerUser = new Action();
-
+  this.registerUser = new Action('/register','POST',( req,res )=>{
+    const userData = req.body;
+    const user = service.createVirtualUser(userData);
+      service
+        .registerAppUser(user)
+        .then(()=>{
+          return res.json(1);
+        })
+        .catch(error => {
+          console.error(error);
+          res.json(error);
+        });
+  },{
+    authRequired: true, roles: ['admin']
+  });
 }
+module.exports = AuthController;
--- services/auth-service.js
+++ services/auth-service.js
@@ -1,20 +1,50 @@
 const DbAccess = require('../DAL/db-access');
-const passport = require('passport');
+const Passport = require('passport');
+const BCrypt = require('bcrypt');
+const AppUser = require('../DAL/AuthModels/AppUser');
 
 const AuthService = function() {
   
   const db = new DbAccess();
 
-  this.secure = function(server){
-    server.use(passport.initialize());
-    server.use(passport.session());
-  }
+  const saltingRounds = 12;
 
-  this.registerAppUser = function() {
-    return db.run('select * from [Order]')
+  this.secure = function(server){
+    server.use(Passport.initialize());
+    server.use(Passport.session());
+  };
+
+
+  this.createVirtualUser = function(userData){    
+    const user = new AppUser();
+    if(!user.isValid(userData))
+    {
+      console.error('Post body does not confrom to registration model...');
+      return null;
+    }
+
+    return new Promise((resolve, reject) => {
+      BCrypt.genSalt(saltingRounds).then((salt) => {
+        console.log(`salt: ${salt}`);
+        console.log(`userData.Password: ${userData.Password}`);
+        BCrypt.hash(userData.Password, salt).then((passwordHash)=>{
+          console.log(`passwordHash: ${passwordHash}`);
+          user.set('Username', userData.username);
+          user.set('PasswordHash', passwordHash);
+          resolve(user);
+        }).catch((err) => reject(err));
+      })
+      .catch((err) => reject(err));
+    });  
+  };
+
+  this.registerAppUser = function(appUser) {
+    this.createVirtualUser().then((user)=>{
+      return db.insert(appUser)
       .catch((error)=> {
         console.error(error);
       });
+    })  
   };
 
 };
