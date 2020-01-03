9a6701948194016b5458385d5c0285c334f5079e
Robert <robert.malina987@gmail.com>
2020-01-03 22:34:54 +0100

    adjustment to use of node's require (in browser, using browserify)

--- Tests.Qunit/db-query-string.test.js
+++ Tests.Qunit/db-query-string.test.js
@@ -1,3 +1,11 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+const AppUser = require('../DAL/AuthModels/AppUser');
+const QueryString = require('../DAL/db-query-string');
+
+QUnit.module('mssql-module sql queries parser');
+
+QUnit.test( "should return valid sql query for INSERT AppUser entity.", function( assert ) {
+  const user = new AppUser();
+  const query = new QueryString().asInsertFor(user);
+  const expectedQuery = "INSERT INTO [dbo].[AppUser] ([Username],[PasswordHash]) VALUES (@username,@passwordhash)";
+  assert.equal( query, expectedQuery );
 });
