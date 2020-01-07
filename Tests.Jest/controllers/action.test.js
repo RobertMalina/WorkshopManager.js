ab06a9d9c7c01eeb59808d088b2055aeae1ced3b
Robert <robert.malina987@gmail.com>
2020-01-07 20:27:51 +0100

    controller root functions unit tests add (JEST)

--- Tests.Jest/controllers/action.test.js
+++ Tests.Jest/controllers/action.test.js
@@ -1,7 +1,50 @@
-// setAsRouteOf test
+const { Action } = require('../../controllers/base/action');
 
+let action1,
+action2,
+action3;
+
+action1 = new Action('/count','GET',()=>{},{});
+action2 = new Action('/of/order/:id','POST',()=>{},{});
+action3 = new Action('/:id','DELETE',()=>{},{});
+
+describe('setAsRouteOf method', () => {
+  describe.each([
+    [action1.path,'orders-controller', action1, 'api/orders' ],
+    [action2.path,'workers-controller', action2, 'api/workers'],
+    [action3.path,'client-controller', action3, 'api/clients']
+  ])('when passed action with path: %s and %s, should return route that contains',
+   (actionPath, controllerFileName, action, expectedPart) => {
+    test(`${expectedPart}`, ()=> {
+      const controllerType = require(`../../controllers/${controllerFileName}`);
+      const controller = new controllerType();
+      controller.pluralize = true;
+      action.setAsRouteOf(controller);
+      expect(action.route).toMatch(`${expectedPart}`);
+    });
+  });
+});
 
 // asRegistrationResult test
+describe('asRegistrationResult method', () => {
+  test('should return expected object when action registered successfully', () => {
+    expect(action1.asRegistrationResult()).toEqual({
+      route: '/api/orders/count',
+      type: 'GET',
+      registered: true,
+      errMsg: null
+    });
+  });
+    test('should return expected object when action register failed', () => {
+      expect(action1.asRegistrationResult({ errMsg: 'some error detected'}))
+      .toEqual({
+        route: '/api/orders/count',
+        type: 'GET',
+        registered: false,
+        errMsg: 'some error detected'
+      });
+  });
+});
 
 
-// checkAction test
+// checkAction
--- Tests.Jest/controllers/controller.test.js
+++ Tests.Jest/controllers/controller.test.js
@@ -1,13 +1,72 @@
-// pluralization in routes check
-// case: pluralize name
-// case: do not pluralize name
+const OrderController = require('../../controllers/orders-controller');
+const WorkerController = require('../../controllers/workers-controller');
+const ClientController = require('../../controllers/client-controller');
+const Controller = require('../../controllers/base/controller');
+const { Action } = require('../../controllers/base/action');
+
+describe('routes root part pluralization', () => {
+
+  let ordersController, rootPart;
+
+  beforeEach( () => {
+    ordersController = new OrderController();
+    console.log('orders controller created...')
+  });
+
+  test('by default should be supported', () => {
+    rootPart = ordersController.getRoutePrefix();
+    expect(rootPart).toEqual('/api/orders');
+  });
+  test('should be not supported', () => {
+    ordersController.pluralize = false;
+    rootPart = ordersController.getRoutePrefix();
+    expect(rootPart).toEqual('/api/order');
+  });
+});
+
+describe('actions gathering: ', () => {
+
+  test('abstract controller should have 0 actions', () => {
+    const abstractController = new Controller({}),
+      actionsCount = abstractController.getActions().length;  
+    expect(actionsCount).toEqual(0);
+  });
+
+  test('abstract controller should have 3 actions', () => {
+    const abstractController = new Controller({});
+    let actionsCount;  
+    abstractController.action1 = new Action('/route1','GET',(req, res) =>{}, {});
+    abstractController.action2 = new Action('/route2','POST',(req, res) =>{}, {});
+    abstractController.action3 = new Action('/route3/:id','GET',(req, res) =>{}, {});
+    actionsCount = abstractController.getActions().length;
+    expect(actionsCount).toEqual(3);
+  });
+
+  describe.each([
+    ['workers-controller'],
+    ['client-controller'],
+    ['orders-controller']
+  ])(`%s `, (controllerFileName) => {
+    test(`shouldn't have 0 actions`, () => {
+      const controllerType = require(`../../controllers/${controllerFileName}`);
+      const controller = new controllerType();
+      let actionsCount = controller.getActions().length;
+      expect(actionsCount).not.toEqual(0);
+    });
+  });
+
+});
 
 // actions gathering check
+
+// arrayContaining(array)
 // case: check actions amount:
 // subcase: OrderController
 // subcase: AuthController
 // case: check if collection item is Action
 
 // api prefixes check
+
+// jest 'toMatch' operator
 // case: api controller
 // case: no api controller
