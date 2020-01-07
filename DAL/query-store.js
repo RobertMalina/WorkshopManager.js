3868d7b853e0e240e185a9b36c5470e8e9268a02
Robert <robert.malina987@gmail.com>
2020-01-07 20:25:57 +0100

    hardcoded queries abandoned due to stored procedures use

--- DAL/query-store.js
+++ DAL/query-store.js
@@ -6,57 +6,15 @@ const QueryStore = function () {
         console.error('Parametr Id jest wymagany!');
         return '';
       }
-      return `
-      SELECT O.Id, 
-      O.SupervisorId, 
-      O.Title, 
-      O.VehicleDescription, 
-      O.Description, 
-      O.DateRegister, 
-      O.DateStart,
-      O.DateEnd, 
-      O.Cost,
-      O.EstimatedTime, 
-      O.Status,
-      (SELECT C.Id FROM [Client] C where C.Id = O.ClientId) AS [Client.Id],
-      (SELECT C.FirstName FROM [Client] C where C.Id = O.ClientId) AS [Client.FirstName],
-      (SELECT C.LastName FROM [Client] C where C.Id = O.ClientId) AS [Client.LastName],
-      (SELECT C.PhoneNumber FROM [Client] C where C.Id = O.ClientId) AS [Client.PhoneNumber],
-	    W.FirstName AS [Supervisor.FirstName],
-      W.LastName AS [Supervisor.LastName],
-      W.PhoneNumber AS [Supervisor.PhoneNumber]
-	    FROM [Order] O
-	    INNER JOIN [Worker] W on O.SupervisorId = W.Id
-	    WHERE O.Id = ${args.id}`;
+      return `EXEC GetOrderDataWithId @orderId=${args.id}`;
     },
     selectOrdersNonArchivized: function(args) {
-      return `
-      SELECT O.Id, 
-      O.SupervisorId, 
-      O.Title, 
-      O.VehicleDescription, 
-      O.Description, 
-      O.DateRegister, 
-      O.DateStart,
-      O.DateEnd, 
-      O.Cost, 
-      O.EstimatedTime, 
-      O.Status,
-      (SELECT C.Id FROM [Client] C where C.Id = O.ClientId) AS [Client.Id],
-      (SELECT C.FirstName FROM [Client] C where C.Id = O.ClientId) AS [Client.FirstName],
-      (SELECT C.LastName FROM [Client] C where C.Id = O.ClientId) AS [Client.LastName],
-      (SELECT C.PhoneNumber FROM [Client] C where C.Id = O.ClientId) AS [Client.PhoneNumber],
-	    W.FirstName AS [Supervisor.FirstName],
-      W.LastName AS [Supervisor.LastName],
-      W.PhoneNumber AS [Supervisor.PhoneNumber]
-	    FROM [Order] O
-	    INNER JOIN [Worker] W on O.SupervisorId = W.Id
-	    WHERE O.Archived = 0`;
+      return `EXEC GetNonArchivedOrdersData`;
     },
-    selectOrdersCountAll: function(args){
+    selectOrdersCountAll: function(args) {
       return 'SELECT COUNT(o.Id) FROM [Order] o';
     },
-    selectOrdersCountNonArchivized: function(args){
+    selectOrdersCountNonArchivized: function(args) {
       return 'SELECT COUNT(o.Id) FROM [Order] o WHERE o.Archived = 0';
     },
     selectOrdersForPagedList: function(args) {
