7e6bb3c40282077b865c1b71a5c9c867fd048a05
Robert Malina <robert.malina987@gmail.com>
2020-02-05 21:19:58 +0100

    JWT token generation support add

--- services/auth-service.js
+++ services/auth-service.js
@@ -142,6 +142,35 @@ const AuthService = function() {
           resolve(decodedToken)
         });
       });
+    },
+    createJWToken: (details) => {
+      if (typeof details !== 'object')
+      {
+        details = {}
+      }
+
+      if (!details.maxAge || typeof details.maxAge !== 'number')
+      {
+        details.maxAge = 3600
+      }
+
+      details.sessionData = _.reduce(details.sessionData || {}, (data, val, key) =>
+      {
+        if (typeof val !== "function" && key !== "password")
+        {
+          data[key] = val
+        }
+        return data;
+      }, {})
+
+      let token = jwt.sign({
+        data: details.sessionData
+        }, process.env.JWT_SECRET, {
+          expiresIn: details.maxAge,
+          algorithm: 'HS256'
+      })
+
+      return token
     }
   };
 
