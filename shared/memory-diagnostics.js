f94a11787da3805c2f432984d8b8f6840664367b
Robert Malina <robert.malina987@gmail.com>
2020-03-10 09:10:32 +0100

    average memory usage counting add

--- shared/memory-diagnostics.js
+++ shared/memory-diagnostics.js
@@ -7,17 +7,19 @@ Object.freeze(units);
 const defPrecision = 2;
 let logsCount = 0;
 
-const print = ({ index, rss, heapTotal, heapUsed }) => {
+const print = ({ index, precision, rss, heapTotal, heapUsed }) => {
+  //result.toFixed(precision)
+
   console.log(
-    `Memory usage log:${index}\n rss: ${rss}\n heapTotal: ${heapTotal}\n heapUsed: ${heapUsed}\n`,
+    `Memory usage log:${index}\n rss: ${rss.toFixed(
+      precision,
+    )}\n heapTotal: ${heapTotal.toFixed(
+      precision,
+    )}\n heapUsed: ${heapUsed.toFixed(precision)}\n`,
   );
 };
 
-const transformAll = (
-  data,
-  targetUnit = units.MB,
-  precision = defPrecision,
-) => {
+const transformAll = (data, targetUnit = units.MB) => {
   if (!Array.isArray(data)) {
     throw new Error(
       'Data param has to be array of { key <string> and bytesCount <int> }.',
@@ -25,12 +27,12 @@ const transformAll = (
   }
   const result = {};
   for (let pair of data) {
-    result[pair.key] = transform(pair.bytesCount, targetUnit, precision);
+    result[pair.key] = transform(pair.bytesCount, targetUnit);
   }
   return result;
 };
 
-const transform = (bytesCount, unit = units.MB, precision = defPrecision) => {
+const transform = (bytesCount, unit = units.MB) => {
   if (bytesCount == 0) {
     return `$0${unit}`;
   }
@@ -49,7 +51,7 @@ const transform = (bytesCount, unit = units.MB, precision = defPrecision) => {
       result = bytesCount / 1024 / 1024;
       break;
   }
-  return `${result.toFixed(precision)} ${unit}`;
+  return result;
 };
 
 const generateLog = function({ rss, heapTotal, heapUsed }) {
@@ -64,26 +66,73 @@ const generateLog = function({ rss, heapTotal, heapUsed }) {
   session.logsCount++;
 
   return {
-    in: function(unit, precision = defPrecision) {
-      const result = transformAll(
+    in: function(unit, precision = defPrecision, verbose = false) {
+      const reads = transformAll(
         [
           { key: 'rss', bytesCount: rss },
           { key: 'heapTotal', bytesCount: heapTotal },
           { key: 'heapUsed', bytesCount: heapUsed },
         ],
         unit,
-        precision,
       );
-      print({ index: session.logsCount, ...result });
+
+      if (typeof session.registerReads == 'function') {
+        session.registerReads(reads);
+      }
+
+      if (verbose) {
+        print({ index: session.logsCount, unit, precision, ...reads });
+      }
     },
   };
 };
 
 const memoryDiagnostics = {
   session: function() {
+    this.reads = {
+      rss: [],
+      heapTotal: [],
+      heapUsed: [],
+    };
+    this.avgs = {
+      rss: 0,
+      heapTotal: 0,
+      heapUsed: 0,
+    };
+    this.registerReads = function(reads) {
+      const { rss, heapTotal, heapUsed } = reads;
+
+      this.reads.rss.push(rss || 0);
+      this.reads.heapTotal.push(heapTotal || 0);
+      this.reads.heapUsed.push(heapUsed || 0);
+
+      this.updateAvgs();
+    };
+    this.updateAvgs = function() {
+      this.avgs.rss =
+        this.reads.rss.reduce((acc, r) => (acc += r)) / this.reads.rss.length;
+      this.avgs.heapTotal =
+        this.reads.heapTotal.reduce((acc, r) => (acc += r)) /
+        this.reads.heapTotal.length;
+      this.avgs.heapUsed =
+        this.reads.heapUsed.reduce((acc, r) => (acc += r)) /
+        this.reads.heapUsed.length;
+    };
+    this.printAvgs = function(precision = defPrecision) {
+      console.log(
+        `Average memory usage (reads count = ${
+          this.logsCount
+        }):\n rss: ${this.avgs.rss.toFixed(
+          precision,
+        )}\n heapTotal: ${this.avgs.heapTotal.toFixed(
+          precision,
+        )}\n heapUsed: ${this.avgs.heapUsed.toFixed(precision)}\n`,
+      );
+    };
     this.logsCount = 0;
     return {
       log: generateLog.bind(this),
+      summarize: this.printAvgs.bind(this),
     };
   },
 };
